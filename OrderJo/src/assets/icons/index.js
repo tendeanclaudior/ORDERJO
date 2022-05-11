c4e150b564d02ef7442b494ddc411ae11f5a876f
hiskiabororing <tommybororing@gmail.com>
2022-05-11 12:38:29 +0800

    menambahkan icons

---
+++ OrderJo/src/assets/icons/index.js
@@ -0,0 +1,11 @@
+import Logo from './Logo.svg';
+import Pic1 from './pic1.svg';
+import Pic2 from './pic2.svg';
+import Pic3 from './pic3.svg';
+import Buy from './Buy.svg';
+import Vector from './Vector.svg';
+import Logohome from './Home.svg';
+import IconBack from './icon-back.svg';
+import ImageProfil from './imageprofil.svg';
+
+export {Logo, Pic1, Pic2, Pic3, Buy, Vector, Logohome, IconBack, ImageProfil};
